fede5302f59a30339ecadaf53f4d8f113a6a9480
Faisal <shahfaisal9240@gmail.com>
2025-07-27 21:08:37 +0530

    My team UI change

--- src/pages/MyTeam/MyTeam.jsx
+++ src/pages/MyTeam/MyTeam.jsx
@@ -3,7 +3,7 @@ import teamData from "./teamData/teamData";
 
 const MyTeam = () => {
   return (
-    <div className="min-h-screen bg-gray-100 py-10 px-4 sm:px-6 lg:px-8 font-sans max-w-4xl mx-auto">
+    <div className="min-h-[85vh] bg-gray-100 py-10 px-4 sm:px-6 lg:px-8 font-sans max-w-4xl mx-auto">
       <h1 className="text-4xl font-extrabold text-indigo-700 mb-10 text-center">
         Our Team
       </h1>
