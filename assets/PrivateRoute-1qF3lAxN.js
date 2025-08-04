66293e780b47b351b6d48a9bd12db81c275147fe
Faisal <shahfaisal9240@gmail.com>
2025-08-04 16:37:43 +0530

    Updates

--- assets/PrivateRoute-1qF3lAxN.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as e,N as o}from"./index-ClbkNrez.js";const n=()=>localStorage.getItem("authToken")?t.jsx(e,{}):t.jsx(o,{to:"/login"});export{n as default};
---
+++ assets/PrivateRoute-CcHx0cn1.js
@@ -0,0 +1,1 @@
+import{j as t,O as e,N as o}from"./index-M4CyN_1V.js";const n=()=>localStorage.getItem("authToken")?t.jsx(e,{}):t.jsx(o,{to:"/login"});export{n as default};
